a19e92e3a2890f6b08415ab130759cd3ba22647e
Frederic Vogels <frederic.vogels@ucll.be>
2020-04-02 19:49:09 +0200

    add-header.rb

--- add-header.rb
+++ add-header.rb
@@ -1,13 +1,14 @@
 require 'find'
 
 
-Find.find('.') do |entry|
+Find.find('docs') do |entry|
     if entry.end_with? '.asciidoc'
         puts "Processing #{entry}"
 
         contents = File.open(entry, :encoding => 'utf-8') do |f|
             f.read
         end
+
         contents.gsub!(%r{\A.*?=}m, '=')
         contents = <<~END + contents
         :tip-caption: 💡
@@ -19,8 +20,3 @@ Find.find('.') do |entry|
         IO.write(entry, contents)
     end
 end
-
-# :tip-caption: 💡
-# :note-caption: ℹ️
-# :important-caption: ⚠️
-# :task-caption: 👨‍🔧
