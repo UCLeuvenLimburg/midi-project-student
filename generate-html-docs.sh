c0eaa5ef125de83baf2cf848911d9e64da3a51b0
Frederic Vogels <frederic.vogels@ucll.be>
2020-04-02 20:52:56 +0200

    generate-html-docs

--- generate-html-docs.sh
+++ generate-html-docs.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+echo Converting AsciiDoc to HTML...
 find -mindepth 1 -name "*.asciidoc" | xargs asciidoctor -R docs -D html
+echo HTML files can be found under html directory
